b2ef70b853bf45c63c9001cc51e9c25e16b10644
huudxx <69554011+huudxx@users.noreply.github.com>
2021-01-31 07:27:12 +0800

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,11 +1,6 @@
 let handler = async m => m.reply(`
 ╭─「 Donasi • Pulsa 」
-│ • Smartfren [088235435804]
-│ • Telkomsel [081357302007]
-│ • Gopay [081357302007]
-╰────
-╭─「 Hubungi 」
-│ > Ingin donasi? Wa.me/6281357302007
+│ Telkomsel [081238556231]
 ╰────
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
