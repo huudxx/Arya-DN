d4f4a42f339f945ae5745b71fc3b409e8249d80d
huudxx <69554011+huudxx@users.noreply.github.com>
2021-01-31 07:23:16 +0800

    Update info.js

--- plugins/info.js
+++ plugins/info.js
@@ -1,28 +1,14 @@
 let handler  = async (m, { conn, usedPrefix: _p }) => {
   conn.reply(m.chat, `
-╠═〘 INFO BOT 〙 ═
-╠➥ Dibuat dengan bahasa javascript via NodeJs
-╠➥ Rec: Drawl Nag
-╠➥ Script: @Nurotomo
+╠═〘 *INFO BOT* 〙 ═
+╠➥ Instagram: @hud0s
+╠➥ Src: Arya Gans
 ║
-╠➥ Github: https://github.com/Arya274/Arya-DN
-╠➥ Instagram: @arpunchs
-╠➥ YouTube: Drawl Nag
-║
-╠═〘 Thanks To 〙 ═
+╠〘 *Thanks To* 〙 ═
 ╠➥ Nurotomo
 ╠➥ MfarelS
 ╠➥ ST4RZ
-╠➥ Dan kawan yang lain :)
-║
-╠═〘 DONASI 〙 ═
-╠➥ SmartFren: 0882-3543-5804
-╠➥ Tsel: 0813-5730-2007
-╠➥ Indosat: 0815-1586-0089
-║
-║>Request? Wa.me/6281357302007
-║
-╠═〘 NfQ BOT 〙 ═
+╠➥ Arya
 `.trim(), m)
 }
 handler.help = ['info']
