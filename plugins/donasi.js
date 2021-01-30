e85a1eb3303670106fcb2b75e0c023e9dddd27ab
huudxx <69554011+huudxx@users.noreply.github.com>
2021-01-31 07:53:22 +0800

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,6 +1,8 @@
 let handler = async m => m.reply(`
 ╭─「 Donasi • Pulsa 」
 │ Telkomsel [081238556231]
+│ Bisa Juga Donasi Tanpa Uang. Klik link -> _https://linkerload.com/2DCMe_  , dan klik salah satu iklan.
+│ Atau Link -> _https://adsafelink.com/gtc5skOZWN_ , dan klik salah satu iklan.
 ╰────
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
